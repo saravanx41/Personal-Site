be61498943e9521d4a42735cf2eee5eec72cafb5
saravanx41 <102127001+saravanx41@users.noreply.github.com>
2024-01-04 15:20:19 +0800

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,10 +8,10 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Saravana`,
+    siteTitleAlt: `Saravana - Profile`,
+    siteHeadline: ``,
+    siteUrl: `https://saravana.xyz`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
